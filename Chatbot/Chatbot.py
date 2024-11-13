48cada199c29e9d929571446b6a9c6e6e5690d09
thelearner411 <91204524+thelearner411@users.noreply.github.com>
2024-11-13 03:05:02 +0100

    create README.md file on instructions to run the app

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -15,31 +15,33 @@ from DTO.ChatMessage import ChatMessage
 from DbConnection.DbConnection import DbConnection
 
 class Chatbot:
-    def __init__(self, user_account_id, history):
+    def __init__(self, chat_id):
         genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
-        self.id = uuid.uuid4()
-        self.user_account = user_account_id
+        self.id = None
+        self.user_account = None
         self.model = genai.GenerativeModel("gemini-1.5-flash")
         self.default_chat_intro = {"role": "model", "parts": "Hello. I am an intelligent chatbot here to help you wih your concerns about PCOS. What's on your mind?"}
-        self.history = history
+        self.history = []
         self.created_at = datetime.datetime.now()
-        if(len(self.history) == 0):
-            self.init_chat()
+        self.db_connection = DbConnection()
+        self.init_chat(chat_id)
     
-    def init_chat(self):
+    def init_chat(self, chat_id):
         try:
-            self.history.append(self.default_chat_intro)
-            history = [] # fetch history
-            self.chat = self.model.start_chat(history=history)
-
-            db_connection = DbConnection()
-            chat = Chat(str(self.id), str(self.user_account), self.created_at)
-            chat_message = ChatMessage(str(uuid.uuid4()), str(self.id), str(self.user_account), "model", self.default_chat_intro["parts"], None, datetime.datetime.now())
-            insert_chat_result = db_connection.insert_chat(chat)
-            insert_chat_message_result = db_connection.insert_chat_message(chat_message)
-            print(chat_message)
-            print(f"Chat initialised: {insert_chat_result['id']}")
-            print(f"Chat first message after initialisation: {insert_chat_message_result['id']}")
+            chat_history = self.parse_chat_messages_from_db(chat_id)
+            if len(chat_history) == 0:
+                self.history.append(self.default_chat_intro)
+                self.chat = self.model.start_chat(history=history)
+                chat = Chat(str(self.id), str(self.user_account), self.created_at)
+                chat_message = ChatMessage(str(uuid.uuid4()), str(self.id), str(self.user_account), "model", self.default_chat_intro["parts"], None, datetime.datetime.now())
+                insert_chat_result = self.db_connection.insert_chat(chat)
+                insert_chat_message_result = self.db_connection.insert_chat_message(chat_message)
+                print(chat_message)
+                print(f"Chat initialised: {insert_chat_result['id']}")
+                print(f"Chat first message after initialisation: {insert_chat_message_result['id']}")
+            else:
+                print(f"Fetching data for existing chat: {chat_id}")
+                self.history = chat_history
 
         except Exception as e:
             print(f"Exception initialising chat: {e}")
@@ -66,13 +68,23 @@ class Chatbot:
                 None,
                 datetime.datetime.now()
             )
-            db_connection = DbConnection()
-            db_connection.insert_chat_message(user_chat_message)
-            db_connection.insert_chat_message(model_chat_message)
+            self.db_connection.insert_chat_message(user_chat_message)
+            self.db_connection.insert_chat_message(model_chat_message)
             return response.text
         
         except Exception as e:
             print(f"Exception querying chat: {e}")
+            return e
+
+    def parse_chat_messages_from_db(self, chat_id):
+        try:
+            chat_messages: list[ChatMessage] = self.db_connection.fetch_chat_messages_by_chat_id(chat_id)
+            history = []
+            for message in chat_messages:
+                history.append({"role": message.sender, "parts": message.message})
+            return history
+        except Exception as e:
+            print(f"Exception parsing chat messages from db: {e}")
             return e
 
 if __name__ == "__main__":
@@ -81,7 +93,7 @@ if __name__ == "__main__":
     
     # Replace with a user_account id that already exists in database for testing the db_connection class
     placeholder_user_account_id = "51288e48-2fee-4881-be9e-7d021df734db"
-    chat = Chatbot(placeholder_user_account_id, [])
+    """chat = Chatbot(placeholder_user_account_id, [])
     print(chat.history[-1])
     end_chat = False
     user_query = input("Query chatbot: ")
@@ -90,4 +102,10 @@ if __name__ == "__main__":
         print(response)
         user_query = input("Query chatbot: ")
         if user_query.strip().lower() == "q":
-            end_chat = True
+            end_chat = True"""
+    
+    
+    placeholder_chat_id = "cffa1a06-8cc7-412c-9539-cdc26fdaf99c" # Replace with existing chat id for testing
+    chat = Chatbot(placeholder_chat_id)
+    chat_messages_by_chat_id = chat.parse_chat_messages_from_db(placeholder_chat_id)
+    print("chat history after parsing: ", chat_messages_by_chat_id)
